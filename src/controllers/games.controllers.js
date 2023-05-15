e96a6f1e371e18672269199d6cd71a969b7b6084
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-05-15 19:00:57 -0300

    feat: schema and post games add

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -11,5 +11,15 @@ export async function findAllGames(req, res) {
 
 
 export async function createGames (req,res) {
-    const {name, image, stockTotal, pricePerDay} = 
-};
+    const {name, image, stockTotal, pricePerDay} = res.body
+
+    try{
+        await db.query(`
+            INSERT INTO games (name, image, "stockTotal", "pricePerDay")
+            VALUES ($1, $2, $3, $4);
+        `, [name, image, stockTotal, pricePerDay ])
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+
+}
--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,12 @@
 import pg from "pg"
 import dotenv from "dotenv"
+
 dotenv.config()
 
 const {Pool} = pg;
 
-const configDatabase = {
-    connnectionString: process.env.DATABASE_URL,
-}
-
-export const db = new Pool(configDatabase)
+export const db = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
 
 
